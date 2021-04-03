ee66df34edc6c26ecf75b7c8dd2978b7681ba077
rubenrufo <ruben.amoros.salvador@gmail.com>
2021-04-03 23:51:27 +0200

    Waves Added
    Beginning of "Spawn Multiple Enemies" Video.

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -6,22 +6,68 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    // Movement
     [SerializeField] float xSpeed = 10f;
     [SerializeField] float ySpeed = 5f;
     [SerializeField] float paddingX = 0.5f;
     [SerializeField] float paddingBottom = 1f;
     [SerializeField] float paddingTopProp = 0.7f;
-
     float xMin;
     float xMax;
     float yMin;
     float yMax;
 
+    // Laser
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float xVelLaser = 0f;
+    [SerializeField] float yVelLaser = 10f;
+    [SerializeField] float laserFiringPeriod = 0.3f;
+    Coroutine fireCoroutine;
+
     // Start is called before the first frame update
     void Start()
     {
         SetUpMoveBoundaries();
-        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fireCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(fireCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(xVelLaser, yVelLaser);
+            yield return new WaitForSeconds(laserFiringPeriod);
+        }
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal")* Time.deltaTime;
+        var deltaY = Input.GetAxis("Vertical")* Time.deltaTime;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX  * xSpeed, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY  * ySpeed, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
     }
 
     private void SetUpMoveBoundaries()
@@ -35,20 +81,7 @@ public class Player : MonoBehaviour
         xMax = xMaxCam - paddingX;
         yMin = yMinCam + paddingBottom;
         yMax = yMinCam + (yMaxCam - yMinCam) * paddingTopProp;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
 
-    private void Move()
-    {
-        var deltaX = Input.GetAxis("Horizontal")* Time.deltaTime;
-        var deltaY = Input.GetAxis("Vertical")* Time.deltaTime;
-        var newXPos = Mathf.Clamp(transform.position.x + deltaX  * xSpeed, xMin, xMax);
-        var newYPos = Mathf.Clamp(transform.position.y + deltaY  * xSpeed, yMin, yMax);
-        transform.position = new Vector2(newXPos, newYPos);
     }
 }
