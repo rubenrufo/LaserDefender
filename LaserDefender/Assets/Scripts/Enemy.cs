5f56bf46d237ff3ac9af880ea3f32063d38e66b2
rubenrufo <ruben.amoros.salvador@gmail.com>
2021-05-02 10:52:44 +0200

    Player and enemies shooting
    Principi Player Life and Death

---
+++ LaserDefender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBtwShots = 0.2f;
+    [SerializeField] float maxTimeBtwShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float xVelLaserRand = 0f;
+    [SerializeField] float yVelLaser = 10f;
+    float xVelLaser;
+    Quaternion laserRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBtwShots, maxTimeBtwShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter=Random.Range(minTimeBtwShots, maxTimeBtwShots);
+        }
+    }
+
+    private void Fire()
+    {
+        xVelLaser = Random.Range(-xVelLaserRand, xVelLaserRand);
+        laserRotation.SetFromToRotation(new Vector3(0, -1, 0), new Vector3(xVelLaser, -yVelLaser, 0));
+        GameObject laser = Instantiate(projectile, transform.position, laserRotation) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(xVelLaser, -yVelLaser);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
